f38567691cac7c6b31b019f9ce7d43039e5c1776
Albérico Eduardo <albericoeduardo202@gmail.com>
2023-09-06 09:01:10 -0300

    content created

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,20 +1,26 @@
 export const dataFake = [
   {
-    'id': 1,
-    'photo': 'aaa',
-    'title': 'saiu novo filme',
+    'id': "1",
+    'photo': 'https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg',
+    'title': 'Textotexto TEXTOTextotexto TEXTO',
+    'description': 'Texto texto Texto textoTexto textoTexto texto Texto texto to textoTexto textoTexto texto Texto textoto textoTexto textoTexto texto Texto texto'
+  },
+  {
+    'id': "2",
+    'photo': 'https://www.healthylifestylesliving.com/wp-content/uploads/2015/12/placeholder-256x256.gif',
+    'title': 'TEXTOTextotexto TEXTOTextotexto',
     'description': 'a'
   },
   {
-    'id': 2,
-    'photo': 'aaa',
-    'title': 'saiu novo filme',
+    'id': "3",
+    'photo': 'https://jakewilson.gallerycdn.vsassets.io/extensions/jakewilson/vscode-placeholder-images/0.1.0/1499508629226/Microsoft.VisualStudio.Services.Icons.Default',
+    'title': 'TEXTOTextotextoextoexto',
     'description': 'a'
   },
   {
-    'id': 3,
-    'photo': 'aaa',
-    'title': 'saiu novo filme',
+    'id': "4",
+    'photo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrdTzaG5FlQmrnYDRDWASDefPqdxaUqSbGNxgukwA4XjLHoTgXhsRQf2VOZU0jgVyiB9A&usqp=CAU',
+    'title': 'TEXT',
     'description': 'a'
   },
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -10,6 +11,7 @@ export class ContentComponent implements OnInit {
   photoCover:string = 'https://via.placeholder.com/500x300'
   contentTitle:string = 'MINHA NOTICIA'
   contentDescription:string = 'ola mnd'
+  private id:string | null= '0'
 
   constructor(
     private route:ActivatedRoute
@@ -17,8 +19,20 @@ export class ContentComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+      this.id = (value.get("id"))
     )
+
+    this.setValuesToComponent(this.id)
   }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photo
+    console.log(this.id)
+  }
+
 
 }
